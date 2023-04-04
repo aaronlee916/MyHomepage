91f39178e55920eef44c8e75f9d2a1bfe0a0b835
Aaron Lee <aaronlee916@qq.com>
2023-04-04 19:04:57 +0800

    1

---
+++ src/userInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Avatar } from 'antd'
+
+class UserInfo extends Component{
+    render(){
+        return(
+            <Avatar src="./images/IMG_0520.PNG" size={256}></Avatar>
+        )
+    }
+}
+export default UserInfo
