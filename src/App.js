b8a9c2fb032a8718d88e25138ae1e6ac1ec358f6
Aaron Lee <aaronlee916@qq.com>
2023-04-05 18:23:47 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
 import UserInfo from './UserInfo';
-let backgroundImage = './images/background.png'
 function App() {
   return (
-    <div className='outerBox' style={{backgroundImage: `url(${backgroundImage})`,height:'1920px',width:'1080px'}}>
+    <div className='outerBox'>
       <UserInfo />
     </div>
   );
--- src/userInfo.js
+++ src/userInfo.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import './UserInfo.css'
-import { Avatar } from 'antd'
+import { Avatar, Button } from 'antd'
 import pic from './images/IMG_0520.PNG'
 class UserInfo extends Component {
     render() {
         return (
             <div className='avatarComponent'>
                 <Avatar src={pic} size={256}></Avatar>
-                <div className='userName'>你好</div>
+                <div className='userName'>Aaron Lee</div>
+                <Button className='enter' onClick={handleEnterClick} block type='primary' style={{height:'50px', top:'50px'}}>进入</Button>
             </div>
         )
     }
+    
+
+}
+function handleEnterClick(){
+    
 }
 export default UserInfo
